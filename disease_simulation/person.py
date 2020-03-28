325e9ff1bbdcece30a4859aec2845b1a3c467493
Rahul Menon <menonrahul02@gmail.com>
2020-03-28 18:20:21 -0500

    Create Person update method

--- disease_simulation/person.py
+++ disease_simulation/person.py
@@ -1,4 +1,9 @@
 class Person:
-    def __init__(self, location, is_infected=False):
+    def __init__(self, location, is_infected=False, days_infected=0):
         self.location = location
         self.is_infected = is_infected
+        self.days_infected = days_infected
+
+    def _update(self):
+        if self.is_infected:
+            self.days_infected += 1
