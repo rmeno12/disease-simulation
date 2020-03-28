5dc01070c1c6db08ee86b1c3115353458451d2bf
Rahul Menon <menonrahul02@gmail.com>
2020-03-27 23:20:17 -0500

    Create Community class

---
+++ disease_simulation/community.py
@@ -0,0 +1,25 @@
+from disease_simulation.person import Person
+import random
+
+
+class Community:
+    def __init__(self, num_people, num_infected, infection_rate, grid_size):
+        self.num_people = num_people
+        self.num_infected = num_infected
+        self.num_healthy = num_people, num_infected
+        self.infection_rate = infection_rate
+        self.grid_size = grid_size
+
+        self.people = self._generatePeople()
+
+    def _generatePeople(self):
+        people = []
+        for _ in range(self.num_infected):
+            x = random.random() * self.grid_size
+            y = random.random() * self.grid_size
+            people.append(Person([x, y], True))
+
+        for _ in range(self.num_healthy):
+            x = random.random() * self.grid_size
+            y = random.random() * self.grid_size
+            people.append(Person([x, y], False))
