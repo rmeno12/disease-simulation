8d8f44825e6256c8aa398262e26074a4d2fc17f3
Rahul Menon <menonrahul02@gmail.com>
2020-03-28 21:34:31 -0500

    Test community update function in main

--- disease_simulation/disease_simulation.py
+++ disease_simulation/disease_simulation.py
@@ -2,7 +2,15 @@ from community import Community
 
 
 def main():
-    print(Community(10, 2, 0.1, 2, 5).people)
+    repetitions = 2
+    community = Community(10, 1, 0.2, 1, 8, 2)
+    for _ in range(repetitions):
+        for key, value in community.sorted_people.items():
+            print(key, value)
+        community.update()
+
+    for key, value in community.sorted_people.items():
+        print(key, value)
 
 
 if __name__ == '__main__':
