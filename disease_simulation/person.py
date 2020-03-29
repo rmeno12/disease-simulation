2066c3fa8c076ca9f99b7095ee5401a3a5bd6045
Rahul Menon <menonrahul02@gmail.com>
2020-03-28 21:33:44 -0500

    Create __repr__ method and rename is_infected to just infected

--- disease_simulation/person.py
+++ disease_simulation/person.py
@@ -1,9 +1,16 @@
 class Person:
-    def __init__(self, location, is_infected=False, days_infected=0):
+    def __init__(self, location, infected=False, days_infected=0,
+                 recovered=False):
         self.location = location
-        self.is_infected = is_infected
+        self.infected = infected
         self.days_infected = days_infected
+        self.recovered = recovered
+
+    def __repr__(self):
+        return 'Person(location='+str(self.location) + ', infected=' + \
+            str(self.infected) + ', days_infected='+str(self.days_infected) + \
+            ', recovered=' + str(self.recovered) + ')'
 
     def _update(self):
-        if self.is_infected:
+        if self.infected:
             self.days_infected += 1
